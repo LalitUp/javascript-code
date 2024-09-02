d1d68d77ac7443cbcc22b33d519fec30613cf41b
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-02 19:23:46 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) ; // code readability should be high
+
+console.log("Lalit"); 
+
+let name = "Lalit" // string data type
+let age = 10 //integer
+let isLoggedIn = false //boolean
+let state; // undefined value
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+// undefined = undefined | null = object
+
+console.log(typeof false)
