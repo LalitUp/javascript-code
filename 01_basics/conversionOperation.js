a1a82562da490a95a2df1bf5250c3ce2d0ef42a7
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-03 17:33:00 +0000

    conversionofDataTypes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -4,3 +4,8 @@ console.log(typeof score);
 
 let valueNumber = Number(score)
 console.log(typeof score);
+
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
