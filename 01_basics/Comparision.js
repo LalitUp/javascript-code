db696c4d0c6c19ac61cb6a59efa91e45b98e4b3d
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-04 17:28:03 +0000

    comparision

---
+++ 01_basics/Comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+
+// logic
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// avoid
+// imp
+
+console.log(null> 0);
+console.log(null == 0);
+console.log(null >=0);
+
+// imp confusing
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,11 +1,18 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueNumber = Number(score)
-console.log(typeof score);
+// let valueNumber = Number(score)
+// console.log(typeof score);
 
 
-let isLoggedIn = 1;
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let isLoggedIn = 1;
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+
+let num1, num2, num3
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
