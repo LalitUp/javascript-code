2960ba1934f9c4c2fd75b052ab45a48c68c73a87
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-12 16:46:14 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,15 @@
+// Stack ( Premitive ), Heap (Non-Primitive)
+
+let myYoutubename = "thelalitthakur"
+
+let anothername = myYoutubename
+anothername = "cartube youtube"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let user = {
+    email: "user at google.com",
+    upi: "user1@ybl"
+}
