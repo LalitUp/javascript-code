6a2e21ea0b94aa3a8ce875541b82aca9f5cff3f5
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-14 04:51:07 +0000

    math and num

---
+++ 01_basics/numbersmath.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.983
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
