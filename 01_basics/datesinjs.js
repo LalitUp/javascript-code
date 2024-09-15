1fa1c108cfe14d8a680974c4aa2f16a85455dfed
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-15 18:16:18 +0000

    dates

---
+++ 01_basics/datesinjs.js
@@ -0,0 +1,11 @@
+// represent single moment 
+
+let myDate = new Date()  // object
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleTimeString);
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toDateString);
+
