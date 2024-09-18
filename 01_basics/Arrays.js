219fd87c8b9b8f77e8ba249aba24acda7d1c5121
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-18 04:12:54 +0000

    Arrays-1

---
+++ 01_basics/Arrays.js
@@ -0,0 +1,15 @@
+// Arrays 
+
+const myArr = [0,1,2,3,4,5]
+const myHeors = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+// Array Methods
+
+// myArr.push(6) //:- Add element in the array
+// myArr.pop()  //:- remove last element from array
+
+// myArr.unshift(9)
+// myArr.shift()
