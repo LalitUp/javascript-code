46671a49c058ad2bc468dddf257768fe4d471f0f
LALIT KUMAR <147649609+LalitUp@users.noreply.github.com>
2024-09-30 10:55:51 +0000

    scope

---
+++ basics-4/scope.js
@@ -0,0 +1,22 @@
+// scope in functions
+
+
+//{} :- scope
+// avoid var mostly
+
+//var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
+
+console.log(a);
+// console.log(b);
