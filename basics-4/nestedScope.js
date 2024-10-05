bd05448cf06bd868ab32f5b2f5d1f3e7b6184e93
LALIT KUMAR <147649609+LalitUp@users.noreply.github.com>
2024-10-05 07:08:09 +0000

    scope

---
+++ basics-4/nestedScope.js
@@ -0,0 +1,37 @@
+
+
+// function one(){
+//     const name = "lalit"
+
+//     function two(){
+//         const website = " YouTube"
+//         console.log(name);
+//     }
+//    // console.log(website);
+
+//     two() 
+// }
+
+// one()
+
+// if(true) {
+//     const username = "lalit"
+//     if(username === "lalit") {
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+
+//     console.log(website)
+
+// }
+
+// console.log(username);
+
+function addone(num){
+    return num+1
+}
+console.log(addone(5))
+
+const addTwo = function(num){
+    return num + 2
+}
